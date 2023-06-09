7c70647279386cfc3d0352fbf26c90474c5af9f1
TryBin03 <try2029138116@163.com>
2023-06-09 18:32:09 +0800

    添加nacos，出现BUG

--- demo-2/src/main/java/com/example/demo2/Demo2Application.java
+++ demo-2/src/main/java/com/example/demo2/Demo2Application.java
@@ -2,8 +2,10 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients //启用OpenFeign
 @SpringBootApplication
 public class Demo2Application {
--- demo-2/src/main/java/com/example/demo2/servier/TestServier.java
+++ demo-2/src/main/java/com/example/demo2/servier/TestServier.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * @author TryBin
  */
-@Component //注册为spring组件，交予IOC容器管理
+@Component(value = "testServier") //注册为spring组件，交予IOC容器管理
 @FeignClient(value = "demo-1") //添加FeignClient注解，绑定服务提供者。
 public interface TestServier {
     @GetMapping("/test")
